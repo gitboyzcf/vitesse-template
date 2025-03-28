4a2c99d9e6e86e286c43ec45a9d24a7739a8167b
gitboyzcf <boyzcf@qq.com>
2025-03-28 15:00:23 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,17 +6,27 @@ import { routes } from 'vue-router/auto-routes'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { en, zhHans } from 'vuetify/locale'
 
 import App from './App.vue'
 
 import '@unocss/reset/tailwind.css'
 import './styles/main.css'
 import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
 import 'uno.css'
 
 const vuetify = createVuetify({
   components,
   directives,
+  icons: {
+    defaultSet: 'mdi', // This is already the default value - only for display purposes
+  },
+  locale: {
+    locale: 'zhHans',
+    fallback: 'sv',
+    messages: { zhHans, en },
+  },
 })
 
 // https://github.com/antfu/vite-ssg
